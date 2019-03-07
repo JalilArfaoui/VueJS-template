3c59d77c207155b6734cd7c3be693daab20b7192
thibaultchazal <chazalthibault@gmail.com>
2019-03-07 10:56:08 +0100

    Debugging architecture Parcours

--- src/components/AdminParcours1.vue
+++ src/components/AdminParcours1.vue
@@ -3,7 +3,7 @@
   <AdminNav />
   <v-layout id="admin-layout" justify-space-around column>
     <v-toolbar flat color="white">
-      <v-toolbar-title>{{this.course.name}}</v-toolbar-title>
+      <v-toolbar-title>{{this.course}}</v-toolbar-title>
       <v-divider
         class="mx-2"
         inset
@@ -326,10 +326,7 @@ export default {
   data () {
     return {
       name: this.$store.state.user.name,
-      course: {
-        id: '',
-        name: 'Parcours 1'
-      },
+      course: 'Parcours 1',
       dialog: false,
       error: '',
       dialogError: '',
@@ -372,7 +369,8 @@ export default {
     }
   },
   created () {
-    this.getCourseId()
+    // this.getCourseId()
+    this.getFirstLevels()
   },
   mounted () {
     if (localStorage.name) {
@@ -383,21 +381,21 @@ export default {
     }
   },
   methods: {
-    async getCourseId () {
-      try {
-        const res = await LevelService.getCourseId({
-          name: this.course.name
-        })
-        this.course.id = res.data._id
-        this.getFirstLevels()
-      } catch (e) {
-        this.error = e.response
-      }
-    },
+    // async getCourseId () {
+    //   try {
+    //     const res = await LevelService.getCourseId({
+    //       name: this.course.name
+    //     })
+    //     this.course.id = res.data._id
+    //     this.getFirstLevels()
+    //   } catch (e) {
+    //     this.error = e.response
+    //   }
+    // },
     async getFirstLevels () {
       try {
         const firstLevels = await LevelService.getFirstLevels({
-          _course: this.course.id
+          course: this.course
         })
         this.firstLevels = Object.keys(firstLevels.data).map((key) => {
           return firstLevels.data[key]
@@ -432,7 +430,7 @@ export default {
         try {
           const res = await LevelService.createFirstLevel({
             name: this.editing.name,
-            _course: this.course.id
+            course: this.course
           })
           if (res.data.firstLevel.name) {
             this.firstLevels.push(res.data.firstLevel)
